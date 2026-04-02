f23e9318352fd3081ca099fab4aa74cb76461cf3
Nazneen@28 <nazneensakheen@gmail.com>
2026-04-02 19:20:55 +0530

    Added Kafka listener implementation

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -3,6 +3,8 @@ package com.jpmc.midascore.component;
 import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.repository.UserRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.kafka.annotation.KafkaListener;
+import com.jpmc.midascore.foundation.Transaction;
 
 @Component
 public class DatabaseConduit {
@@ -15,5 +17,15 @@ public class DatabaseConduit {
     public void save(UserRecord userRecord) {
         userRepository.save(userRecord);
     }
+    @KafkaListener(topics = "${kafka.topic}", groupId = "midas-group")
+public void listen(Transaction transaction) {
+     System.out.println("Transaction amount: " + transaction.getAmount());
 
+    UserRecord userRecord = new UserRecord(
+        String.valueOf(transaction.getSenderId()),
+        transaction.getAmount()
+    );
+
+    userRepository.save(userRecord);
+}
 }
